45511629a2040b0624e925ab2ee7b1dfdb9c28c5
Uchijo <kgtnttki.yuta@gmail.com>
2023-06-11 21:11:44 +0900

    探索部分のバグ修正

--- main.go
+++ main.go
@@ -7,9 +7,33 @@ import (
 )
 
 func main() {
-	input := "ab"
-	result := search(input, "1", true)
+	hoge := model.RegUnion{
+		Left:  model.RegString{Content: "a"},
+		Right: model.RegString{Content: "b"},
+	}
+	states, out, err := hoge.ToStates("hoge")
+	if err != nil {
+		panic("something went wrong")
+	}
+	fmt.Printf("out: %v\n", out)
+	endState := model.State{
+		Id: out,
+		Moves: []model.Move{},
+		IsEnd: true,
+	}
+	states = append(states, endState)
+	for i, v := range states {
+		fmt.Printf("%v: %v\n", i, v)
+	}
+	fmt.Println("")
+
+	input := "a"
+	result := search(input, "hoge", true, states)
 	fmt.Printf("result: %v\n", result)
+
+	// input := "aaa"
+	// result := search(input, "1", true, states)
+	// fmt.Printf("result: %v\n", result)
 }
 
 type log struct {
@@ -22,9 +46,10 @@ func (l log) alike(log log) bool {
 	return l.buffer == log.buffer && l.id == log.id && l.moveTo == log.moveTo
 }
 
+// 既に訪れた状態を記憶して無限ループを防ぐ
 var tried = []log{}
 
-func search(input, currentStateId string, first bool) bool {
+func search(input, currentStateId string, first bool, states []model.State) bool {
 	// fmt.Printf("current buffer: %v\n", input)
 	// fmt.Printf("current stateId: %v\n", currentStateId)
 
@@ -40,19 +65,19 @@ func search(input, currentStateId string, first bool) bool {
 	}
 
 	// 入力なしでゴール状態なら終わり
-	// 入力なしでゴール状態じゃなかったら終わり
+	// 入力なしでゴール状態じゃなかったらイプシロンづたいにゴールにたどり着けるか確認
 	if input == "" {
-		return currentState.IsEnd
+		return searchEpsilonGoal(currentStateId, states, false)
 	}
 
 	// 行ける可能性があるところをリストアップ
 	for _, move := range currentState.Moves {
 		if move.IsEpsilon || move.Input == input[:1] {
-			if first {
-				fmt.Println(" === ")
-				fmt.Printf("trying move: %v\n", move.MoveTo)
-				fmt.Printf("is epsilon : %v\n", move.IsEpsilon)
-			}
+			// if true {
+			// 	fmt.Printf("trying move: %v\n", move.MoveTo)
+			// 	fmt.Printf("is epsilon : %v\n", move.IsEpsilon)
+			// 	fmt.Println(" === ")
+			// }
 
 			result := false
 			if move.IsEpsilon {
@@ -63,7 +88,7 @@ func search(input, currentStateId string, first bool) bool {
 					}
 				}
 				tried = append(tried, log{currentStateId, input, move.MoveTo})
-				result = search(input, move.MoveTo, false)
+				result = search(input, move.MoveTo, false, states)
 			} else {
 				nextLog := log{currentStateId, input, move.MoveTo}
 				for _, l := range tried {
@@ -72,7 +97,7 @@ func search(input, currentStateId string, first bool) bool {
 					}
 				}
 				tried = append(tried, log{currentStateId, input, move.MoveTo})
-				result = search(input[1:], move.MoveTo, false)
+				result = search(input[1:], move.MoveTo, false, states)
 			}
 			if result {
 				return true
@@ -80,6 +105,52 @@ func search(input, currentStateId string, first bool) bool {
 			continue
 		}
 	}
+	return false
+}
+
+// 既に訪れた状態を記憶して無限ループを防ぐ
+var triedEpsilon = []log{}
+
+func searchEpsilonGoal(currentId string, states []model.State, recursiveCall bool) bool {
+	// ログ初期化
+	if !recursiveCall {
+		triedEpsilon = []log{}
+	}
+
+	// 現在の状態がゴールならtrueで終わり
+	var currentState model.State
+	for _, v := range states {
+		if v.Id == currentId {
+			currentState = v
+			break
+		}
+	}
+	if currentState.IsEnd {
+		return true
+	}
+
+	// ゴールじゃないならIsEpsilonなmoveを探索
+	for _, v := range currentState.Moves {
+		if !v.IsEpsilon {
+			continue
+		}
+
+		currentLog := log{id: currentId, buffer: "", moveTo: v.MoveTo}
+		for _, vv := range triedEpsilon {
+			if vv.alike(currentLog) {
+				return false
+			}
+		}
+		triedEpsilon = append(triedEpsilon, currentLog)
+		hasGoalAhead := searchEpsilonGoal(v.MoveTo, states, true)
+		if hasGoalAhead {
+			return true
+		} else {
+			// 次のループへ
+		}
+	}
+
+	// 上に該当しないならこの状態からはゴールに辿り着けない
 	return false
 }
 
